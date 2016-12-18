46b37ed333cbc2b891cf15a881f7007f060272b4
mku <mkuhn@gmx.ch>
2016-12-18 14:46:58 +0100

    add htdocs

---
+++ htdocs/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="de">
+    <head>
+        <title>SA_Test</title>
+        <meta name="description" content="SA Test">
+        <meta name="author" content="Markus Kuhn">
+        <link rel="shortcut icon" type="image/x-icon" href="/images/arbeit.ico"/>
+        <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="/css/sa_test.css">
+    </head>
+    <body>
+
+        <div>
+            <p>Hallo SA</p>
+            <p>Hallo GitHub</p>
+        </div>
+
+    </body>
+</html>
